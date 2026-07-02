e40249169928063890c150562056ecbfc7defb56
N02R <nh9338743@gmail.com>
2026-07-02 14:04:27 +0300

    update HomeService.php

---
+++ app/Services/HomeService.php
@@ -0,0 +1,40 @@
+<?php
+
+class HomeService
+{
+    public static function getHero($conn, $lang)
+    {
+        $slug = 'home_hero';
+
+        $stmt = $conn->prepare("
+            SELECT * FROM pages 
+            WHERE slug = ? AND lang = ?
+        ");
+
+        $stmt->bind_param("ss", $slug, $lang);
+        $stmt->execute();
+
+        return $stmt->get_result()->fetch_assoc();
+    }
+
+    public static function getServices($conn, $lang)
+    {
+        $stmt = $conn->prepare("
+            SELECT * FROM service_items 
+            WHERE lang = ?
+        ");
+
+        $stmt->bind_param("s", $lang);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        $data = [];
+
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+
+        return $data;
+    }
+}
